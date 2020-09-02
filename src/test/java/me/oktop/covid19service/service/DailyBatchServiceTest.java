a605f557ae3870658bd1a0d1ff71fd7ddcda2969
oktopman <colaws3@gmail.com>
2020-09-02 23:51:56 +0900

    리팩토링으로 인한 테스트코드 수정

--- src/test/java/me/oktop/covid19service/service/DailyBatchServiceTest.java
+++ src/test/java/me/oktop/covid19service/service/DailyBatchServiceTest.java
@@ -1,6 +1,7 @@
 package me.oktop.covid19service.service;
 
-import me.oktop.covid19service.dto.WebhookRequest;
+import me.oktop.covid19service.client.SlackWebhookClient;
+import me.oktop.covid19service.dto.DailyBoardResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -21,24 +24,48 @@ class DailyBatchServiceTest {
     private DailyBatchService dailyBatchService;
 
     @Test
-    @DisplayName("지역발생 감염자 호출 테스트")
-    void covid19_area_occur_test() throws URISyntaxException {
+    @DisplayName("지역발생 감염자 데이터 조회후 슬랙 웹훅 테스트")
+    void covid19_area_occur_select_after__slack_webhook_test() throws URISyntaxException {
         dailyBatchService.covid19BoardSchedule();
     }
 
     @Test
-    @DisplayName("코로나소식 슬랙채널 웹훅 테스트")
+    @DisplayName("코로나소식 지역발생 감염자 호출 성공시 웹훅 테스트")
     void corona_notice_slack_webhook_test() {
         //given
-        String message = "누적 확진자 : 3,961명\n" +
-                "전일 대비 증감 : \t(+94)\n" +
-                "격리중: \t1,937명\n" +
-                "누적 격리해제: \t2,001명\n" +
-                "사망자: \t23명\n";
+        DailyBoardResponse response = new DailyBoardResponse();
+        DailyBoardResponse.Body body = new DailyBoardResponse.Body();
+        DailyBoardResponse.Items items = new DailyBoardResponse.Items();
+        List<DailyBoardResponse.Item> itemList = new ArrayList<>();
+        DailyBoardResponse.Item item = new DailyBoardResponse.Item();
+        item.setDefCnt("");
+        item.setIncDec("");
+        item.setIsolIngCnt("");
+        item.setIsolClearCnt("");
+        item.setDeathCnt("");
+        itemList.add(item);
+        items.setItem(itemList);
+        body.setItems(items);
+        response.setBody(body);
         //when
-        HttpStatus httpStatus = dailyBatchService.webhookToSlack(new WebhookRequest(message));
+        HttpStatus status = SlackWebhookClient.send(response);
         //then
-        assertTrue(httpStatus.is2xxSuccessful());
+        assertTrue(status.is2xxSuccessful());
+    }
+
+    @Test
+    @DisplayName("코로나 지역발생 감염자 호출 실패시 웹훅 테스트")
+    void corona_notice_fail_slack_webhook_test() {
+        //given
+        DailyBoardResponse response = new DailyBoardResponse();
+        DailyBoardResponse.Body body = new DailyBoardResponse.Body();
+        DailyBoardResponse.Items items = new DailyBoardResponse.Items();
+        body.setItems(items);
+        response.setBody(body);
+        //when
+        HttpStatus status = SlackWebhookClient.send(response);
+        //then
+        assertTrue(status.is2xxSuccessful());
     }
 
 }
